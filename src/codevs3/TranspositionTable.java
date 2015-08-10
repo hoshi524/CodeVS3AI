c37fc9f3eabd5efbec5fb60d640169d1f426cfa4
hoshi <hoshimamoru@team-lab.com>
2015-08-10 23:01:45 +0900

    t2.microで動くようにメモリ使用量を減らす

--- src/codevs3/TranspositionTable.java
+++ src/codevs3/TranspositionTable.java
@@ -3,9 +3,9 @@ package codevs3;
 import java.util.Arrays;
 
 public class TranspositionTable {
-	private static final int SIZE = 1 << 26;
+	private static final int SIZE = 1 << 25;
 	private static final int MASK = SIZE - 1;
-	private static final int BUFFER_SIZE = 1 << 20;
+	private static final int BUFFER_SIZE = 1 << 19;
 
 	private Next[] buffer = new Next[BUFFER_SIZE];
 	private Next[] table = new Next[SIZE];
